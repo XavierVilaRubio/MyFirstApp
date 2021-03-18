161055ebb89b25e322f1139837e2c0904f7fda6d
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2021-03-18 12:53:21 +0100

    Initial commit

---
+++ app/src/main/java/dam/invisere/gtidic/udl/cat/myfirstapp/FirstFragment.java
@@ -0,0 +1,63 @@
+package dam.invisere.gtidic.udl.cat.myfirstapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+
+    TextView showCountTextView;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+        return fragmentFirstLayout;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast myToast = Toast.makeText(getActivity(), "Hello Toast!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countMe(view);
+            }
+
+            private void countMe(View view) {
+                String countString = showCountTextView.getText().toString();
+                Integer count = Integer.parseInt(countString);
+                count++;
+                showCountTextView.setText(count.toString());
+            }
+        });
+
+        view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentCount = Integer.parseInt(showCountTextView.getText().toString());
+                FirstFragmentDirections.ActionFirstFragmentToSecondFragment action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount);
+                NavHostFragment.findNavController(FirstFragment.this).navigate(action);
+            }
+        });
+    }
+}
